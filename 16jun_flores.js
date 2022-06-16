428b9918317883b4dd199cef20a207ab87212dca
smougs <miltonmrq@gmail.com>
2022-06-16 10:02:37 -0300

    first commit

---
+++ 16jun_flores.js
@@ -0,0 +1,54 @@
+// User = {
+//     nombre: "Milton",
+//     edad: 18,
+//     email: "miltonmrq@gmai.com",
+//     vivo: true,
+// }
+
+/*     function mostrar() {
+        document.write("<h1>Hola " + User.nombre + "</h1>");
+        document.write("<h1>Tienes " + User.edad + " años</h1>");
+        document.write("<h1>Tu email es " + User.email + "</h1>");
+        document.write("<h1>Estas " + (User.vivo ? "vivo" : "muerto") + "</h1>");
+    }
+
+    mostrar()
+ */
+
+
+//     class User {
+//         constructor(nombre, edad, email, vivo) {
+//             User.nombre = nombre;
+//             User.edad = edad;
+//             User.email = email;
+//             User.vivo = vivo;
+//         }
+//     }
+
+//     console.log(construccion = new User("Nelson", 17, "NelsonAghemo@gmail.com", true));
+
+// console.log(User.nombre);
+// console.log(User.edad);
+// console.log(User.email);
+// console.log(User.vivo);
+
+
+/* class Producto {
+    constructor(nombre, precio, cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+}
+
+console.log(construccion = new Producto("Leche", 10, 10));
+console.log(construccion = new Producto("Huevos", 10, 10)); 
+ */
+const productos {
+    {name:'Leche', price: 10, quantity: 10},
+    {name:'Huevos', price: 10, quantity: 10},
+    {name:'Pan', price: 10, quantity: 10},
+
+}
+
+console.log(productos);
